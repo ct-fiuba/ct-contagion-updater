6df0f4434fe614c316e92e2b4380b3736077c23e
Nacho Raik <ignacioraiksg@gmail.com>
2021-06-25 19:16:59 -0300

    Perdon oli

--- go.mod
+++ go.mod
@@ -3,5 +3,6 @@ module github.com/ct-fiuba/ct-contagion-updater
 go 1.14
 
 require (
-    github.com/streadway/amqp v1.0.0 // indirect
+	github.com/streadway/amqp v1.0.0
+	go.mongodb.org/mongo-driver v1.5.2
 )
--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
-	"github.com/streadway/amqp"
+	"context"
+	"fmt"
 	"log"
 	"os"
+
+	"github.com/streadway/amqp"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type Rule struct {
+	Index         int    `bson:"index"`
+	ContagionRisk string `bson:"contagionRisk"`
+	DurationCmp   string `bson:"durationCmp,omitempty"`
+	DurationValue int    `bson:"durationValue,omitempty"`
+	M2Cmp         string `bson:"m2Cmp,omitempty"`
+	M2Value       int    `bson:"m2Value,omitempty"`
+	SpaceValue    string `bson:"spaceValue,omitempty"`
+}
 
 func failOnError(err error, msg string) {
 	if err != nil {
@@ -15,8 +32,22 @@ func failOnError(err error, msg string) {
 func main() {
 	queueAddress := os.Getenv("QUEUE_ADDRESS")
 	queueName := os.Getenv("QUEUE_NAME")
-	log.Printf("Trying to connect to the RabbitMQ queue %s, at address %s", queueName, queueAddress)
+	dbUri := os.Getenv("MONGODB_URI")
 
+	ctx, cancel := context.WithCancel(context.Background())
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(dbUri))
+	if err != nil {
+		panic(err)
+	}
+	defer client.Disconnect(ctx)
+	defer cancel()
+
+	log.Printf("Connected to the DB!")
+
+	database := client.Database("contact-tracing-db")
+	rulesCollection := database.Collection("rules")
+
+	log.Printf("Trying to connect to the RabbitMQ queue %s, at address %s", queueName, queueAddress)
 	conn, err := amqp.Dial(queueAddress)
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
@@ -25,17 +56,17 @@ func main() {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	// _, err = ch.QueueDeclare(
-	// 	queueName, // name
-	// 	false,     // durable
-	// 	false,     // delete when unused
-	// 	false,     // exclusive
-	// 	false,     // no-wait
-	// 	nil,       // arguments
-	// )
-	// if err != nil {
-	// 	log.Printf("WARNING: Issues found declaring queue: %s", err)
-	// }
+	_, err = ch.QueueDeclare(
+		queueName, // name
+		true,      // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+	if err != nil {
+		log.Printf("WARNING: Issues found declaring queue: %s", err)
+	}
 
 	msgs, err := ch.Consume(
 		queueName, // queue
@@ -53,6 +84,15 @@ func main() {
 	go func() {
 		for d := range msgs {
 			log.Printf(">>> Consumed a message: %s", d.Body)
+			var rules []Rule
+			cursor, err := rulesCollection.Find(ctx, bson.D{})
+			if err != nil {
+				panic(err)
+			}
+			if err = cursor.All(ctx, &rules); err != nil {
+				panic(err)
+			}
+			fmt.Println(rules)
 		}
 	}()
 
